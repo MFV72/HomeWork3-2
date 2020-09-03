7ce9a0f3921c6937c32e8aa7a7456adb1bd93f8c
mfv <mit.fedor@gmail.com>
2020-09-03 22:53:13 +0500

    add bonus cacculation

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,19 @@
 public class Main {
+    public static void main(String[] args) {
+        int balance = 100;
+        int minimumLimit = 1000;
+        int payment;
+        int bonus;
+        int count;
+        count = 100; //1 рубль бонуса за 100 переведенных
+        payment = 1100;
+        if (payment > minimumLimit) {
+            bonus = payment / count;
+        } else {
+            bonus = 0;
+        }
+        balance = balance + payment + bonus;
+        System.out.println(balance);
+        System.out.println(bonus);
+    }
 }
