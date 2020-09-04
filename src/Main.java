6073f12b8bcfd71d543784a78c7257e6328bf37d
mfv <mit.fedor@gmail.com>
2020-09-04 22:28:02 +0500

    fix issue

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        int balance = 100;
         int minimumLimit = 1000;
-        int payment;
+        int payment = 1100;
         int bonus;
-        int count;
-        count = 100; //1 рубль бонуса за 100 переведенных
-        payment = 1100;
+        int count = 100; //1 рубль бонуса за 100 переведенных
         if (payment > minimumLimit) {
             bonus = payment / count;
         } else {
             bonus = 0;
         }
-        balance = balance + payment + bonus;
+        int balance = 100;
+        balance += payment + bonus;
         System.out.println(balance);
         System.out.println(bonus);
     }
